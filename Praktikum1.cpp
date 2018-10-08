e2055b5b69e77c6993a24de4e76650676cb20186
Marinska <43942718+Marinska@users.noreply.github.com>
2018-10-08 10:39:25 +0700

    Create Praktikum1.cpp

---
+++ Praktikum1.cpp
@@ -0,0 +1,15 @@
+//menghitung luas segitiga
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,t,l;
+	cout<<"masukan nilai alas"<<endl;
+	cin>>a;
+	cout<<"masukan nilai tinggi"<<endl;
+	cin>>t;
+	l=a*t/2;
+	cout<<"luas segitiga adalah "<<l<<endl;
+	
+}
