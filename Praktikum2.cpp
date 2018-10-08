823311701b622133e192211b7389257208feeb82
Marinska <43942718+Marinska@users.noreply.github.com>
2018-10-08 10:31:14 +0700

    Create Praktikum2.cpp
    Mencari bilangan genap dan ganjil

---
+++ Praktikum2.cpp
@@ -0,0 +1,14 @@
+//mencari bilangan genap/ganjil
+
+#include <iostream>
+using namespace std;
+int main(){
+
+int bilangan;
+cout<<"masukan bilangan:"<<endl;
+cin>>bilangan;
+if (bilangan%2==0)
+cout<<"bilangan genap"<<endl;
+else
+cout<<"bilangan ganjil"<<endl;
+}
